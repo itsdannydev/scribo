932530e655af4e6c07b4615057a06bcc73727d3b
itsdannydev <itsdanny.dev@gmail.com>
2026-06-27 15:05:49 +0530

    added preview and production channels for OTA updates

--- app.config.js
+++ app.config.js
@@ -1,13 +1,23 @@
 module.exports = ({ config }) => {
   const buildAbi = process.env.BUILD_ABI;
-  if (buildAbi) {
-    return {
-      ...config,
-      plugins: [
-        ...(config.plugins || []),
-        ['./plugins/withAbiFilters', { abis: [buildAbi] }],
-      ],
+  const isPreview = process.env.PREVIEW_BUILD === 'true';
+
+  let result = { ...config };
+
+  if (isPreview) {
+    result.name = 'Scribo (Preview)';
+    result.android = {
+      ...result.android,
+      package: 'com.danny.scribo.preview',
     };
   }
-  return config;
+
+  if (buildAbi) {
+    result.plugins = [
+      ...(result.plugins || []),
+      ['./plugins/withAbiFilters', { abis: [buildAbi] }],
+    ];
+  }
+
+  return result;
 };
